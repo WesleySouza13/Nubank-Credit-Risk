8203054b78ecc0c71c20b377d0e3d21203772204
WesleySouza13 <souzabusiness13@gmail.com>
2025-08-15 17:06:18 -0300

    subindo mudanças e poda em arvore

--- FeatureEng.py
+++ FeatureEng.py
@@ -1,25 +1,33 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 import pandas as pd
 import numpy as np
+from optbinning import OptimalBinning
 from binarizer import binarizer
-#from binarizer import binarizer
+
 class FeatureEng(BaseEstimator, TransformerMixin):
     def __init__(self):
         self.bin = binarizer(col='income')
+        self.optb = None  # aqui vamos guardar o optimal binning
 
-    def fit(self, x, y=None):
+    def fit(self, x, y):
         self.bin.fit(x, y)
+
+        # criar o OptimalBinning para risk_rate
+        self.optb = OptimalBinning(name='risk_rate', dtype="numerical",  min_bin_size=0.05, solver="cp")
+        self.optb.fit(x['risk_rate'].values, y)
+
         return self
 
     def transform(self, x):
         df = x.copy()
-    
-        
-        # criar novas variaveis : score3, n_issues 
+
+        # aplicar binarizer
+        df = self.bin.transform(df)
+
+        # criar novas variáveis
         df['n_bankruptcies_flag'] = (df['n_bankruptcies'] > 1).astype(int)
-        #df['score_per_issue'] = df['score_3'] * df['n_issues']
-        #df['score_per_bankruptcies_flag'] = df['score_3'] * df['n_bankruptcies_flag']
-        #df['log_n_bankruptcies'] = np.log1p(df['n_bankruptcies'])
-        #df['score_per_bankruptcies'] = df['score_3'] / (1 + df['n_bankruptcies'])
+
+        # aplicar optimal binning
+        df['risk_rate_bin'] = self.optb.transform(df['risk_rate'].values, metric="woe")  # ou "bins" se quiser o número do bin
 
         return df
